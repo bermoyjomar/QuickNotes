00262f577c6248127c22a65d16917c8ebe1a8c26
bermoyjomar <bermoyjomar00@gmail.com>
2025-10-21 12:53:56 +0800

    Update

--- App.js
+++ App.js
@@ -1,15 +1,15 @@
+//App.js
 import React from 'react';
 import { StatusBar } from 'react-native';
-import Header from './Header';
-import SearchBar from './SearchBar';
-import ActiveFilters from './ActiveFilters';
-import FiltersModal from './FiltersModal';
-import NoteInput from './NoteInput';
-import NoteCard from './NoteCard';
-import EmptyState from './EmptyState';
-import { storageService } from './storage';
-import { colors } from './colors';
 import HomeScreen from './HomeScreen';
+import { colors } from './colors';
+import ActiveFilters from './ActiveFilters';
+import EmptyState from './EmptyState';
+import FiltersModal from './FiltersModal';
+import Header from './Header';
+import NoteCard from './NoteCard';
+import NoteInput from './NoteInput';
+import SearchBar from './SearchBar';
 
 export default function App() {
   return (
